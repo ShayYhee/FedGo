1ffd026692eda6d0127241a3aa489d53a98b1cef
ShayYhee <faithosebi02@gmail.com>
2024-11-07 00:32:39 +0300

    correct file_path and grid_data  urls in models_utils.py

--- backendFedGo/utils/model_utils.py
+++ backendFedGo/utils/model_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b79842f747c151a638d0921ab1cfcefd953898c0bd07fafb4c21f8ddfa72cd69
-size 3672
+oid sha256:4ab26e20167cc94ff248b8db72e71e58d6a954ee2917ffce313baec404286d01
+size 3784
