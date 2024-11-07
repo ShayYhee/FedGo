606260b4535af21fa87427df588cef86aaeff4a5
ShayYhee <faithosebi02@gmail.com>
2024-11-07 05:20:12 +0300

    changed database url

--- backendFedGo/utils/db_utils.py
+++ backendFedGo/utils/db_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbf10e3a396e17882144e30598ff0640649427b5694b5c2960abd03dfd559079
-size 8668
+oid sha256:44ca50e5dcbefb56a553416efd36fd61a10340ce3ff8bcd57c31974e57afbaef
+size 8802
